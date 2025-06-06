d0d637edf53d35264f4cc0896eaa3cf81f330b38
talibd <talibkhan111105@gmail.com>
2025-06-06 12:50:12 +0530

    change port 5000 to 8080

--- main.py
+++ main.py
@@ -429,4 +429,4 @@ def download_image(url, dest_path):
     return False
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
